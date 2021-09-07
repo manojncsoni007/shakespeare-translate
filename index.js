529a9b4cab67a875f24fcb245bbf5370dbd22373
Manoj Soni <manojncsoni@gmail.com>
2021-09-07 17:37:09 +0530

    Features changes

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-console.log('yeah this is connected');
+let inputText = document.querySelector('#txt-input');
+let outputBox = document.querySelector('#txt-output');
+let translateButton = document.querySelector('#translate');
 
 let url = 'https://api.funtranslations.com/translate/shakespeare.json';
+
+function eventHandler() {
+    let text = inputText.value;
+    console.log('clicked it');
+    console.log(text);
+
+}
+
+translateButton.addEventListener('click', eventHandler);
