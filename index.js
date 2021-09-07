45e44b1dc79045e5d99100747768a93cef09a1af
Manoj Soni <manojncsoni@gmail.com>
2021-09-07 18:12:08 +0530

    final push

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ let translateButton = document.querySelector('#translate');
 
 let url = 'https://api.funtranslations.com/translate/shakespeare.json';
 
-function eventHandler() {
-    let text = inputText.value;
-    console.log('clicked it');
-    console.log(text);
 
+function urlTranslator(text) {
+    return url + "?" + "text=" + text;
 }
 
-translateButton.addEventListener('click', eventHandler);
+function errorHandler(error) {
+    console.log('error occured, error')
+}
+
+function clickEventHandler() {
+    let text = inputText.value;
+
+    fetch(urlTranslator(text))
+        .then(response => response.json())
+        .then(data =>
+            outputBox.innerText = data.contents.translated)
+        .catch(errorHandler);
+}
+
+
+translateButton.addEventListener('click', clickEventHandler);
